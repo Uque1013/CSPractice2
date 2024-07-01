d148f4f1c83d21374e27e2174d26019c9ddc350d
y2eun <kye6735@naver.com>
2024-07-02 03:47:39 +0900

    #6 23-7. 새도잉

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -8,8 +8,12 @@ namespace CSPractice2
 {
     internal class Program
     {
+        public static int number = 10;
         static void Main(string[] args)
         {
+            // #6 23-7. 새도잉과 하이딩
+            int number = 20;
+            Console.WriteLine(number);
         }
     }
 } 
